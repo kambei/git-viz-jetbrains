bd8c87ace271160be7304c7a646a150df35e48ac
Andrea 'kambei' Bovi <and.bovi@gmail.com>
2025-10-03 11:18:21 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    id("org.jetbrains.intellij.platform") version "2.1.0"
+}
+
+group = "dev.kambei"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform { defaultRepositories() }
+}
+
+dependencies {
+    intellijPlatform { 
+        intellijIdeaCommunity("2024.2")
+        bundledPlugins("Git4Idea")
+        instrumentationTools()
+    }
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.9.0.202403050737-r")
+    testImplementation(kotlin("test"))
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        id.set("dev.kambei.gitviz")
+        name.set("GitViz Horizontal")
+        version.set(project.version.toString())
+        vendor {
+            name.set("kambei")
+            url.set("https://github.com/kambei/git-viz-jetbrains")
+        }
+        description = "Shows the Git history horizontally with author, commit message, SHA, tags, and branches."
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
